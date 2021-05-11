4b07d7e49673d3911e6f86efa771bb3c48fc2ef2
23carnies <46233990+23carnies@users.noreply.github.com>
2021-05-11 13:00:58 -0400

    Initail commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio of Karen Lobin Perkins`,
+    description: `Front End Engineer, Karen Lobin Perkins, resume, example work, and more.`,
+    author: `@23carnies`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
