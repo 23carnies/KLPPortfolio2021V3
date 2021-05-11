e3e275e2d0acf334f2b0c72dc8b58cc2480f05c6
23carnies <46233990+23carnies@users.noreply.github.com>
2021-05-11 17:04:41 -0400

    Add header, devIcons to Intro

--- src/components/sections/intro.js
+++ src/components/sections/intro.js
@@ -4,26 +4,56 @@ import { below, Flex } from '../utilities';
 import { Heading, SubHeading } from '../elements/headings';
 import styled from 'styled-components';
 import Iceland from '../../images/klo-iceland.jpg';
+import { Btn } from '../elements/buttons';
+import { Css3, Dot, JavaScript, ReactLogo, StyledComponents, W3c } from '../utilities/Icons';
+import Header from '../layouts/header';
 
 const Intro = () => {
   return (
     <SectionIntro>
-      <Image src={Iceland} alt="Karen in Iceland" />
+      <Header />
+      {/* <Image src={Iceland} alt="Karen in Iceland" /> */}
       <TextBox>
-        <Heading>Hi! I'm Karen</Heading>
-        <SubHeading>
+        <Headline>Hi! I'm Karen</Headline>
+        <SubHeadLine>
           I am a software engineer based in Atlanta, focused on Front End design
           in ReactJS, useability, and accessability.
-        </SubHeading>
+        </SubHeadLine>
       </TextBox>
+      <button>See My Work</button>
+      <div>
+        <ReactLogo/>
+        <Dot />
+        <Css3 />
+        <Dot />
+        <W3c />
+        <Dot />
+        <JavaScript />
+        <Dot />
+        <StyledComponents />
+
+      </div>
     </SectionIntro>
   );
 };
 
 export default Intro;
 
+const Headline = styled(Heading)`
+  color: white;
+  text-align: right;
+`;
+const SubHeadLine = styled(SubHeading)`
+  color: white;
+  text-align: right;
+`;
+
 const SectionIntro = styled(Section)`
-  ${Flex};
+  background-image: url(${Iceland});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  ${Flex({fd:'column'})};
   border-bottom: 10px solid ${props => props.theme.tagLineColor};
   padding-bottom: 2%;
   ${below.small`
@@ -31,16 +61,16 @@ const SectionIntro = styled(Section)`
   `}
 `;
 
-const Image = styled.img`
-  border-radius: 60%;
-  width: 30%;
-  margin: 1%;
-  border: 2px inset ${props => props.theme.pageBackground};
-  ${below.small`
-    width: 80%;
-    margin: 5% 0;
-  `}
-`;
+// const Image = styled.img`
+//   border-radius: 60%;
+//   width: 30%;
+//   margin: 1%;
+//   border: 2px inset ${props => props.theme.pageBackground};
+//   ${below.small`
+//     width: 80%;
+//     margin: 5% 0;
+//   `}
+// `;
 
 const TextBox = styled.div`
   width: 50%;
