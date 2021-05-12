384aa2789c03b19601feddbb1c2e06f9ebd41bc8
23carnies <46233990+23carnies@users.noreply.github.com>
2021-05-12 09:48:17 -0400

    Remove border-bottom

--- src/components/layouts/section.js
+++ src/components/layouts/section.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Flex } from '../utilities';
 
 export const Section = styled.section`
-  border-bottom: 10px solid ${props => props.theme.tagLineColor};
+  /* border-bottom: 10px solid ${props => props.theme.tagLineColor}; */
   padding-bottom: 2%;
   ${Flex({ fd: 'column', ai: 'center', jc: 'center' })};
 `;
