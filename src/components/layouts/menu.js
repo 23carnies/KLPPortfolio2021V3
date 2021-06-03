76b7abd112ff308576b0ddd93e0c89c2d6fee0ec
23carnies <46233990+23carnies@users.noreply.github.com>
2021-06-03 12:54:15 -0400

    Correct menu background color

--- src/components/layouts/menu.js
+++ src/components/layouts/menu.js
@@ -52,7 +52,7 @@ export default Menu;
 
 const NavWrapper = styled.div`
 
-    background: ${white};
+    background: radial-gradient(101.23% 226.86% at 105.57% 106.78%, rgba(183, 0, 139, 0.34) 10.42%, rgba(160, 22, 168, 0.958021) 84.25%), linear-gradient(123.03deg, rgba(255, 202, 67, 0.6) 6.71%, rgba(255, 211, 97, 0) 52.29%), #E76F2B;;
     color: ${white};
 `;
 
