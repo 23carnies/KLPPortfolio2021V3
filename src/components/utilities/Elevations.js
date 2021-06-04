0fbd3c6a5607ae7f2814511729a5afbc2455c3a0
23carnies <46233990+23carnies@users.noreply.github.com>
2021-06-04 12:29:27 -0400

    Shadow styling

--- src/components/utilities/Elevations.js
+++ src/components/utilities/Elevations.js
@@ -5,5 +5,5 @@ export const btn_active =
 
 export const box_shadow1 = `inset 0 4px 9px -9px ${darkGrey}`;
 export const box_shadow2 =
-  '0 1px 9px  rgba(0,0,0, 0.32), 0 1px -2px rgba(0,0,0,0.42)';
+  '2px 1px 9px  rgba(0,0,0, 0.8)';
 export const box_shadow4 = `inset 0 4px 9px -9px ${pinkBlack}`;
